55033bbbe0649f8e47c72ccab5120e58f34849a9
ht <taoabc@gmail.com>
2020-04-18 00:22:37 +0800

    add antd

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import { Button, Input } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.component(Button.name, Button);
+Vue.component(Input.name, Input);
+Vue.component(Input.Password.name, Input.Password);
 
 Vue.config.productionTip = false;
 
---
+++ packages/ui/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <h1>Hello</h1>
+    <a-input placeholder="用户名" allowClear />
+    <br />
+    <br />
+    <a-input-password placeholder="密码" allowClear />
+    <a-button type="primary">登录</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.login {
+  border: 1px;
+  width: 100px;
+  height: 100px;
+}
+</style>
