0650041e5f690d7217bb32e4dc44cc33fb83c697
huhaitao <huhaitao@100tal.com>
2020-05-09 14:54:29 +0800

    update ui version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
 export default {
-  token: 'bbbbec28ebdd4547839b7125310788ca', // TODO remove it
   jwtKey: 'fce72f8f-bab6-4391-8a4b-91756ee37922', // key for jwt sign
   port: 8001, // port
   admin: [
